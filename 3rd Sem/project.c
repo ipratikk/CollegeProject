5d1365467f108988093b7772a87c913371fff5b0
Pratik Goel <ipratikgoel@gmail.com>
2018-10-22 13:10:55 +0530

    porject updated and working

--- 3rd Sem/project.c
+++ 3rd Sem/project.c
@@ -8,7 +8,16 @@ typedef struct Directory
 	char filetype[30];
 	struct Directory *link[100];
 }dir;
-dir *root=NULL,currentDirectory=NULL;
+void commands();								//displays all commands
+void folder(dir *, char [], char []);			//creates new file/folder in the current directory 
+void display(dir *, int);						//displays all the files and folders recursively
+void ls(dir *);									//displays the contents of current directory
+bool alreadyExists(dir *, char [], char []);	//checks if a given file/folder already exists in the current directory
+void showCurrentDirectory(dir *);				//shows the name of thecurrent directory
+void changeCurrentDirectory(dir *, char []);	//changes the working directory
+void setRoot();									//sets current directory to the root folder
+dir *root = NULL;		//the root directory
+dir *currentDirectory;	//the current directory
 int main()
 {
 	int i;
@@ -20,34 +29,143 @@ int main()
 		newfolder->link[i]=NULL;
 	root=newfolder;
 	currentDirectory = root;
-	char input[110];
-	char command[5];
+	char command[10];
 	char filename[100];
 	printf("File Management Service\n");
-	printf("enter \"help\" for help\n");
-	scanf("%[^\n]%*c",input);
-	extract(input,command,filename);
+	printf("enter \"help all\" for help\n");
+	scanf("%s%s",command,filename);
 	while(1)
 	{
-		if(strcmp(command,"mkdir")==0)
-			create(currentDirectory,filename,"folder");
-		if(strcmp(command,"file")==0)
-			create(currentDirectory,filename,"file");
-		if(strcmp(command,"tree")==0)
-			display(root);
-		
+		if(strcmp(command, "mkdir") == 0)
+			folder(currentDirectory, filename, "folder");
+		else if(strcmp(command, "file") == 0)
+			folder(currentDirectory, filename, "file");
+		else if(strcmp(command, "tree") == 0)
+			display(root, 0);
+		else if(strcmp(command, "ls") == 0)
+			ls(currentDirectory);
+		else if(strcmp(command, "sd") == 0)
+			showCurrentDirectory(currentDirectory);
+		else if(strcmp(command, "cd") == 0)
+			changeCurrentDirectory(currentDirectory, filename);
+		else if(strcmp(command, "help") == 0)
+			commands();
+		else if(strcmp(command, "exit") == 0)
+			exit(0);
+		else
+			printf("Invalid command.\n");
+		scanf("%s%s",command,filename);
 	}
+	return 0;
 }
 void commands()
 {
 	printf("mkdir <foldername> : making a new folder\n");
 	printf("file <filename> : create a new file\n");
-	printf("tree : display all the files and folders in the root directory\n");
-	printf("ls : display all the files and folders in the current directory\n");
-	printf("sd : show current directory\n");
-	pritnf("cd <foldername> : change current directory\n");
+	printf("tree al : display all the files and folders in the root directory\n");
+	printf("ls -al : display all the files and folders in the current directory\n");
+	printf("sd view: show current directory\n");
+	printf("cd <foldername> : change current directory\n");
 	printf("cd root : go to root directory\n");
-	printf("help : get the list of commands\n");
-	printf("exit : to exit the console\n");
+	printf("help all: get the list of commands\n");
+	printf("exit 0: to exit the console\n");
+	printf("Commands are specific to this program and not to be confused with DOS/Linux terminal commands\n");
+	printf("----represents a folder\n");
+	printf("~~~~represents a file\n\n");
 }
-	
+void folder(dir *temp, char filename[30], char filetype[10])
+{
+	//check for duplicate filename
+	if(alreadyExists(temp, filename, filetype))
+	{
+		printf("A %s with this name already exists.\n", filetype);
+		return;
+	}
+	int i = 0;
+	//loop to enter the first link which is set to NULL
+	for(i=0; i<100; i++)
+		if(temp->link[i] == NULL)
+			break;
+	dir *nf;	//new file/folder
+	nf = (dir*) malloc(sizeof(dir));
+	temp->link[i] = nf;	//link the new folder to ith position of temp's link
+	strcpy(nf->filetype, filetype);	//copy the filetype to nf
+	strcpy(nf->filename, filename);	//copy the filename to nf
+	//set new file/folders links to NULL
+	for(i=0; i<100; i++)
+		nf->link[i] = NULL;
+	printf("%s has been successfully created under %s\n", nf->filename, temp->filename);
+}
+void ls(dir *temp)
+{
+	int i;	//loop variable
+	for(i=0; i<100 && temp->link[i] != NULL; i++)
+		printf("%s\t", temp->link[i]->filename);
+	printf("\n");
+}
+void display(dir *temp, int level)
+{
+    if(temp == NULL)
+        return;
+	int i;	//loop variable
+	//print '-' for folders
+	if(strcmp(temp->filetype, "folder") == 0)
+		for(i=1; i <= level; i++)
+			printf("---");
+	//print '~' for files
+	else if(strcmp(temp->filetype, "file") == 0)
+		for(i=1; i <= level; i++)
+			printf("~~~");
+	printf("%s\n", temp->filename);
+	//call all the subdirectories/files recursively
+	for(i=0; i<100 && temp->link[i] != NULL; i++)
+		display(temp->link[i], level+1);	//increment level to print more dashes
+}
+bool alreadyExists(dir *temp, char name[30], char type[6])
+{
+	int i;
+	for(i=0; i<100 && temp->link[i] != NULL; i++)
+		if(strcmp(temp->link[i]->filetype, type) == 0)
+			if(strcmp(temp->link[i]->filename, name) == 0)
+				return true;
+	return false;
+}
+void showCurrentDirectory(dir *cur)
+{
+    printf("The current directory is: %s\n", cur->filename);
+}
+void changeCurrentDirectory(dir *temp, char foldername[30])
+{
+	//to set current directory to "root"
+	if(strcmp(foldername, "root") == 0)
+	{
+		setRoot();
+		showCurrentDirectory(currentDirectory);
+		return;
+	}
+	int i; //loop variable
+	bool found = false;
+	for(i=0; i<100 && temp->link[i] != NULL; i++)
+	{
+		if(strcmp(temp->link[i]->filetype, "folder") == 0)
+			if(strcmp(temp->link[i]->filename, foldername) == 0)
+			{
+				found = true;
+				currentDirectory = temp->link[i];
+				break;
+			}
+	}
+	//if directory does not exist, create one and set current directory to it
+	if(found == false)
+	{
+		printf("Directory not found. Creating new directory.\n");
+		folder(currentDirectory, foldername, "folder");
+		changeCurrentDirectory(currentDirectory, foldername);
+		return;
+	}
+	showCurrentDirectory(currentDirectory);
+}
+void setRoot()
+{
+	currentDirectory = root;
+}
