c3d503af6ac47d958a148170dd0bcdbc49517f06
Pratik Goel <ipratikgoel@gmail.com>
2018-10-30 18:15:27 +0530

    updated madlibs and added ppt

--- 3rd Sem/madlibs.py
+++ 3rd Sem/madlibs.py
@@ -1,8 +1,16 @@
+'''
+
+Madlibs- a fun game with unknown words to form a story!!
+
+Soltuion by - Pratik Goel (@ipratikk)
+
+'''
+
+
 import random
 def getKeys(formatString):
-    '''formatString is a format string with embedded dictionary keys.
-    Return a set containing all the keys from the format string.'''
-
+    '''formatString is a string with dictionary keys.
+    Return a set containing all the keys from the string.'''
     keyList = list()
     end = 0
     repetitions = formatString.count('{')
@@ -11,33 +19,28 @@ def getKeys(formatString):
         end = formatString.find('}', start)
         key = formatString[start : end]
         keyList.append(key) # may add duplicates
-
     return set(keyList) # removes duplicates: no duplicates in a set
 
 def addPick(cue, dictionary): # from madlibDict.py
     '''Prompt for a user response using the cue string,
-    and place the cue-response pair in the dictionary.
-    '''
+    and place the cue-response pair in the dictionary.'''
     promptFormat = "Enter a specific example for {name}: "
     prompt = promptFormat.format(name=cue)
     response = input(prompt)
     dictionary[cue] = response                                                             
 
-
 def getUserPicks(cues):
     '''Loop through the collection of cue keys and get user choices.
-    Return the resulting dictionary.
-    '''
+    Return the resulting dictionary.'''
     userPicks = dict()
     for cue in cues:
         addPick(cue, userPicks)
-    return userPicks   
+    return userPicks
 
 def tellStory(storyFormat):
-    '''storyFormat is a string with Python dictionary references embedded,
-    in the form {cue}.  Prompt the user for the mad lib substitutions
-    and then print the resulting story with the substitutions.
-    '''
+    '''storyFormat is a string with dictionary references,
+    in the form {cue}. Prompt the user for the mad lib cues
+    and then print the resulting story with the cues.'''
     cues = getKeys(storyFormat)
     userPicks = getUserPicks(cues)
     story = storyFormat.format(**userPicks)
