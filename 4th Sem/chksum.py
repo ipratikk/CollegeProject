83acbc10e3ea475bb627bfe883af4d55ecf16203
Pratik <ipratikgoel@gmail.com>
2019-03-29 18:15:23 +0530

    Checksum.py updated

--- 4th Sem/chksum.py
+++ 4th Sem/chksum.py
@@ -1,8 +1,8 @@
-def generate(string):
+def generate(string,m):
     l=len(string)
     sum=0
-    for i in range(len(send),0,-m):
-        x=int(send[i-m:i],2)
+    for i in range(len(string),0,-m):
+        x=int(string[i-m:i],2)
         sum+=x
         x=bin(sum)[2:]
         if len(x)>m:
@@ -14,32 +14,45 @@ def generate(string):
 def flip(c): 
     return '1' if (c == '0') else '0'
 
-print("Checksum Generator")
-print("------------------")
-print("\nEnter number of segments of data")
-k=int(input())
-print("Enter number of bits in each segment")
-m=int(input())
-print("\nEnter the sender data")
-send=input()
-sender=generate(send)
-tmp=["0" for i in range(m-len(sender))]
-tmp+=str(sender)
-sender=[flip(tmp[i]) for i in range(m)]
-sender="".join(sender)
-print("\nThe sender checksum is : %s\n\n"%sender)
-print("Checksum Checker")
-print("----------------")
-print("\nEnter the receiver data")
-receive=input()
-receiver=generate(receive)
-tmp=["0" for i in range(m-len(receiver))]
-tmp+=str(receiver)
-receiver="".join(tmp)
-print("\nThe receiver checksum is : %s\n"%receiver)
-chksum=str(bin((int(sender,2)+int(receiver,2)))[2:])
-print("The combined checksum is : %s"%chksum)
-if chksum.count('1')==m:
-    print("Data Transmission Successful")
-else:
-    print("Not Successful")
+def solve():
+	print("Checksum Generator")
+	print("------------------")
+	print("\nEnter number of segments of data")
+	k=int(input())
+	print("Enter number of bits in each segment")
+	m=int(input())
+	print("\nEnter the sender data")
+	send=input()
+	sender=generate(send,m)
+	tmp=["0" for i in range(m-len(sender))]
+	tmp+=str(sender)
+	sender=[flip(tmp[i]) for i in range(m)]
+	sender="".join(sender)
+	print("\nThe sender checksum is : %s\n\n"%sender)
+	print("Checksum Checker")
+	print("----------------")
+	print("\nEnter the receiver data")
+	receive=input()
+	receiver=generate(receive,m)
+	tmp=["0" for i in range(m-len(receiver))]
+	tmp+=str(receiver)
+	receiver="".join(tmp)
+	print("\nThe receiver checksum is : %s\n"%receiver)
+	chksum=str(bin((int(sender,2)+int(receiver,2)))[2:])
+	tmp=['0' for i in range(m-len(chksum))]
+	tmp+=chksum
+	chksum="".join(tmp)
+	print("The combined checksum is : %s"%chksum)
+	if chksum.count('1')==m:
+	    print("Data Transmission Successful")
+	else:
+	    print("Not Successful")
+	    
+while True:
+	solve()
+	print("Do you want to enter more? Y/N")
+	x=input()
+	if x=='n' or x=='N':
+		print("Thank You")
+		break
+
