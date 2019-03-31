6bd006d5e0c828bb173364b9fd6bdccb87f45f32
Pratik <ipratikgoel@gmail.com>
2019-03-31 19:35:36 +0530

    Changed Chksum a little

--- 4th Sem/chksum.py
+++ 4th Sem/chksum.py
@@ -23,22 +23,26 @@ def solve():
 	m=int(input())
 	print("\nEnter the sender data")
 	send=input()
+	data=send
 	sender=generate(send,m)
 	tmp=["0" for i in range(m-len(sender))]
 	tmp+=str(sender)
 	sender=[flip(tmp[i]) for i in range(m)]
 	sender="".join(sender)
-	print("\nThe sender checksum is : %s\n\n"%sender)
+	print("\nThe sender checksum is : %s\n"%sender)
+	print("\nThe sender data is : %s\n\n"%(data+sender))
 	print("Checksum Checker")
 	print("----------------")
 	print("\nEnter the receiver data")
 	receive=input()
+	data2=receive[len(receive)-4:]
+	receive=receive[:len(receive)-4]
 	receiver=generate(receive,m)
 	tmp=["0" for i in range(m-len(receiver))]
 	tmp+=str(receiver)
 	receiver="".join(tmp)
-	print("\nThe receiver checksum is : %s\n"%receiver)
-	chksum=str(bin((int(sender,2)+int(receiver,2)))[2:])
+	print("\nThe receiver checksum is : %s\n"%(receive+receiver))
+	chksum=str(bin((int(data2,2)+int(receiver,2)))[2:])
 	tmp=['0' for i in range(m-len(chksum))]
 	tmp+=chksum
 	chksum="".join(tmp)
