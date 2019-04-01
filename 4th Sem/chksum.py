dba7e3d04bb381a85b567924f3ba600bad7cf5c9
Pratik <ipratikgoel@gmail.com>
2019-04-01 09:06:29 +0530

    changed chksum.py

--- 4th Sem/chksum.py
+++ 4th Sem/chksum.py
@@ -14,13 +14,9 @@ def generate(string,m):
 def flip(c): 
     return '1' if (c == '0') else '0'
 
-def solve():
+def solve(k,m):
 	print("Checksum Generator")
 	print("------------------")
-	print("\nEnter number of segments of data")
-	k=int(input())
-	print("Enter number of bits in each segment")
-	m=int(input())
 	print("\nEnter the sender data")
 	send=input()
 	data=send
@@ -31,32 +27,44 @@ def solve():
 	sender="".join(sender)
 	print("\nThe sender checksum is : %s\n"%sender)
 	print("\nThe sender data is : %s\n\n"%(data+sender))
+def solve2(k,m):
 	print("Checksum Checker")
 	print("----------------")
 	print("\nEnter the receiver data")
 	receive=input()
-	data2=receive[len(receive)-4:]
-	receive=receive[:len(receive)-4]
-	receiver=generate(receive,m)
-	tmp=["0" for i in range(m-len(receiver))]
-	tmp+=str(receiver)
-	receiver="".join(tmp)
-	print("\nThe receiver checksum is : %s\n"%(receive+receiver))
-	chksum=str(bin((int(data2,2)+int(receiver,2)))[2:])
-	tmp=['0' for i in range(m-len(chksum))]
-	tmp+=chksum
-	chksum="".join(tmp)
-	print("The combined checksum is : %s"%chksum)
-	if chksum.count('1')==m:
-	    print("Data Transmission Successful")
+	if len(receive)!=(k+1)*m:
+		print("Incorrect Inputs")
 	else:
-	    print("Not Successful")
+		data2=receive[len(receive)-4:]
+		receive=receive[:len(receive)-4]
+		receiver=generate(receive,m)
+		tmp=["0" for i in range(m-len(receiver))]
+		tmp+=str(receiver)
+		receiver="".join(tmp)
+		print("\nThe receiver checksum is : %s\n"%(receive+receiver))
+		chksum=str(bin((int(data2,2)+int(receiver,2)))[2:])
+		tmp=['0' for i in range(m-len(chksum))]
+		tmp+=chksum
+		chksum="".join(tmp)
+		print("The combined checksum is : %s"%chksum)
+		if chksum.count('1')==m:
+			print("Data Transmission Successful\n\n")
+		else:
+			print("Not Successful\n\n")
 	    
 while True:
-	solve()
-	print("Do you want to enter more? Y/N")
-	x=input()
-	if x=='n' or x=='N':
-		print("Thank You")
+	print("0.Exit")
+	print("1.Checksum Generator")
+	print("2.Checksum Checker")
+	ch=int(input())
+	if ch==0:
 		break
-
+	else:
+		print("\nEnter number of segments of data")
+		k=int(input())
+		print("Enter number of bits in each segment")
+		m=int(input())
+		if ch==1:
+			solve(k,m)
+		else:
+			solve2(k,m)
